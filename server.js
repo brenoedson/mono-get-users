fe82d2612d34ebc6e24970945829d310bc3106ee
breno <brenoedson@gmail.com>
2026-08-08 11:47:06 -0300

    feat: implementar interface de programacao de aplicacao minimalista

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import http from 'node:http';
 
-http.createServer(() => {
-    console.log('Knocked on the door.');
+const users = [{
+    name: 'Breno',
+    email: 'brenoedson@gmail.com'
+}, {
+    name: 'Linus',
+    email: 'linusedson201011@gmail.com'
+}, {
+    name: 'Alex',
+    email: 'alex@gmail.com'
+}];
+
+
+
+http.createServer((request, response) => {
+    response.writeHead(200, {"content-type": "application/json"});
+    response.end(JSON.stringify(users));
 }).listen(3000);
