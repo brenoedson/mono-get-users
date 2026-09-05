73c0647accb97be3b5508cef4ab8c196711b3528
breno <brenoedson@gmail.com>
2026-09-05 11:55:13 -0300

    feat: implementar acesso de usuario cadastrado nos dados mocados

--- client/app.js
+++ client/app.js
@@ -1,21 +1,72 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const paragraph = document.querySelector('p');
+//const input = document.querySelector('input');
+const btnIndex = document.querySelector('#btn-index');
+const btnLogin = document.querySelector('#btn-login');
+const btnHome = document.querySelector('#btn-home');
 
-button.addEventListener('click', async (event) => {
-    event.preventDefault();
+const email = document.querySelector('input[type="email"]');
+const password = document.querySelector('input[type="password"]');
+//const paragraph = document.querySelector('p');
 
-    const inputValue = input.value;
-    // console.log(input.value);
-    input.value = '';
+if (btnIndex){
+    btnIndex.addEventListener('click',() => (window.location.href = './pages/login.html'));
+}
 
-    const response = await fetch('http://127.0.0.1:3000/users');
-    const data = await response.json();
+if (btnLogin){
+    // async e await são comandos necessários para definir rotinas assíncronas, que podem demorar para responder
+    // dessa forma, a rotina esperará a resposta antes de prosseguir com o script
+    btnLogin.addEventListener('click', async (event) => {
+        // Precisamos prevenir o comportamento padrão do botão, o 'submit'.
+        // O botão está dentro de um formulário, e o padrão, ao clicar no botão, é pegar os dados do formulário e enviar para
+        // algum canto, mas esse canto ainda não existe, essa nova página ainda não foi criada.
+        // Para evitar isso, nós utilizamos a rotina preventDefault
+        event.preventDefault();
 
-    const user = data.find(user => user.email === inputValue);
+        const emailValue = email.value;
+        const passwordValue = password.value;
 
-    if(!user){
-        return paragraph.textContent = `Usuário não encontrado!`;
-    }
-    paragraph.textContent = `Usuário ${user.name} encontrado!`;
-});
+        const response = await fetch('http://localhost:3000/users');
+        // pegar a resposta da requisição fetch e extrair os dados no formato JSON
+        const data = await response.json();
+
+        const user = data.find((user) => (
+            user.email === emailValue && user.password === passwordValue
+        )); // no find() acima usamos parênteses em vez das chaves {} porque dessa forma fica dispensado o uso do return
+
+        //Caso email ou senha não sejam encontrados, exibe a mensagem de email e senha inválidos
+        // Caso seja encontrado, exibe 'acesso permitido' e vai para a nova página
+        if(!user) return window.alert('Email e/ou senha inválidos!');
+        window.alert('Acesso permitido!');
+        window.location.href = './home.html';
+
+        console.log(emailValue, passwordValue, data);
+    });
+}
+
+if(btnHome){
+    btnHome.addEventListener('click', () => {
+        setTimeout(() => {},0);
+        setTimeout(() => {},1000);
+        setTimeout(() => {},2000);
+        window.location.href = './login.html';
+    });
+}
+
+
+
+// button.addEventListener('click', async (event) => {
+//     event.preventDefault();
+
+//     const inputValue = input.value;
+//     // console.log(input.value);
+//     input.value = '';
+
+//     const response = await fetch('http://127.0.0.1:3000/users');
+//     const data = await response.json();
+
+//     const user = data.find(user => user.email === inputValue);
+
+//     if(!user){
+//         return paragraph.textContent = `Usuário não encontrado!`;
+//     }
+//     paragraph.textContent = `Usuário ${user.name} encontrado!`;
+// });
--- server/src/mocks/user.mock.js
+++ server/src/mocks/user.mock.js
@@ -1,12 +1,15 @@
 const users = [{
     name: 'Breno',
-    email: 'brenoedson@gmail.com'
+    email: 'brenoedson@gmail.com',
+    password: '123456'
 }, {
     name: 'Linus',
-    email: 'linusedson201011@gmail.com'
+    email: 'linusedson201011@gmail.com',
+    password: '123456'
 }, {
     name: 'Alex',
-    email: 'alex@gmail.com'
+    email: 'alex@gmail.com',
+    password: '123456'
 }];
 
 export default users;
